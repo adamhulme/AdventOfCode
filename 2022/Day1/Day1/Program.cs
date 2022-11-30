5559920aa9ffb2b0ea14f929f2e1f84fffd5523f
Adam Hulme <adam.hulme@enable.com>
2022-11-30 10:50:51 +0000

    Setup Day1 2022

---
+++ 2022/Day1/Day1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Day1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var p = new Program();
+            var input = File.ReadAllLines(@"C:\Projects\AoCgithub\AdventOfCode\2022\Day1\input.txt");
+
+            Console.WriteLine(input[0][0]);
+            Console.WriteLine(input[1][1]);
+        }
+    }
+}
