cc93845c1dcb47fbe2dad0d6ed5672ba876f8ecf
Adam Hulme <adam.hulme@enable.com>
2022-12-01 12:04:57 +0000

    Day 1

--- 2022/Day1/Day1/Program.cs
+++ 2022/Day1/Day1/Program.cs
@@ -12,8 +12,30 @@ namespace Day1
             var p = new Program();
             var input = File.ReadAllLines(@"C:\Projects\AoCgithub\AdventOfCode\2022\Day1\input.txt");
 
-            Console.WriteLine(input[0][0]);
-            Console.WriteLine(input[1][1]);
+            var currentElf = 0;
+            var elves = new List<int>();
+
+            for (int i=0; i<input.Length; i++)
+            {
+                if (input[i] != "")
+                {
+                    currentElf += Int32.Parse(input[i]);
+                }
+                else
+                {
+                    elves.Add(currentElf);
+                    currentElf = 0;
+                }
+            }
+
+
+            // Part 1
+            Console.WriteLine(elves.Max());
+
+            // Part 2
+            elves.Sort();
+            elves.Reverse();
+            Console.WriteLine(elves[0] + elves[1] + elves[2]);
         }
     }
 }
